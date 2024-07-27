2cb8d75618222a7868ae8c1fb6dba8e09ec2acad
SeyoungCho <wnl383@naver.com>
2024-07-28 03:30:48 +0900

    feat: add Patient type

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,15 @@ export type SigninSchema = {
   email: string;
   password: string;
 };
+
+export type Patient = {
+  id: number;
+  profileImage: string;
+  DOB: string;
+  numberOfVisits: number;
+  name: string;
+  symptom: string;
+  recentVisitDate: string;
+  severityPercentage: string;
+  status: "HIGH_RISK" | "MODERATE_RISK" | "LOW_RISK" | "NORMAL";
+};
