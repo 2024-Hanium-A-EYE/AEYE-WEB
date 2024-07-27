2a51d3d17ff68fb91bd7350e0e450ed298e06019
SeyoungCho <wnl383@naver.com>
2024-07-28 00:49:26 +0900

    feat: 로그인 페이지 퍼블리싱

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className={cn(Pretendard.className, "h-dvh w-dvw")}>
+      <body className={cn(Pretendard.className, "min-w-[1440px] h-dvh")}>
         {children}
       </body>
     </html>
--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,3 +1,4 @@
+import SigninForm from "@/components/auth/SigninForm";
 import Image from "next/image";
 import React from "react";
 
@@ -5,17 +6,17 @@ const SigninPage = () => {
   return (
     <main className="flex h-full min-w-full">
       <section className="flex h-full flex-1 shrink-0 items-center justify-center">
-        로그인 화면
+        <SigninForm />
       </section>
       <section className="flex flex-1 shrink-0 items-center justify-center rounded-bl-[12.5rem] bg-[linear-gradient(135deg,_rgba(207,232,255,1)_0%,_rgba(255,255,255,1)_100%)]">
         <div className="flex flex-col items-center">
-          <div className="relative mb-[32px] aspect-[276/65] w-[276px]">
+          <div className="relative mb-[32px] aspect-[276/65] w-[210px]">
             <Image src="/images/logo-compound-horizontal.png" alt="로고" fill />
           </div>
-          <h2 className="text-3xl font-semibold text-primary">
+          <p className="text-xl font-semibold text-[#2B3674]">
             진료 데이터의 새로운 눈, AEYE
-          </h2>
-          <div className="relative mt-[68px] aspect-[693.78/553.75] w-[600.78px]">
+          </p>
+          <div className="relative mt-[68px] aspect-[693.78/553.75] w-[450.78px]">
             <Image src="/images/docs.png" alt="랜딩 이미지" fill />
           </div>
         </div>
