7eb886d04df35d82b14557a634e4f644412d9e5f
SeyoungCho <wnl383@naver.com>
2024-07-28 15:16:55 +0900

    feat: add checkup List

--- src/app/(main)/patients/[id]/page.tsx
+++ src/app/(main)/patients/[id]/page.tsx
@@ -1,5 +1,8 @@
 import { Patient } from "@/types";
 import { notFound } from "next/navigation";
+import Image from "next/image";
+import CheckupTable from "@/components/patients/CheckupTable";
+import CheckupList from "@/components/patients/CheckupList";
 
 export async function generateStaticParams() {
   const patients: Patient[] = await fetch(
@@ -28,11 +31,32 @@ const PatientDetailPage = async ({ params }: { params: { id: string } }) => {
   return (
     <main className="m-auto max-w-[1440px] px-8">
       <section>
-        <h1>{patient.name}</h1>
-        <p>Date of Birth: {patient.DOB}</p>
-        <p>Symptom: {patient.symptom}</p>
-        <p>Status: {patient.status}</p>
-        {/* Add more details as needed */}
+        <h1 className="mb-12 text-3xl font-semibold">{`${patient.name} 님의 진료 기록`}</h1>
+        <div className="flex h-40 w-full gap-10 rounded-xl border p-5">
+          <div className="relative size-28 overflow-hidden rounded-full">
+            <Image
+              src={patient.profileImage}
+              alt="환자 사진"
+              fill
+              className="object-cover"
+            />
+          </div>
+          <div className="flex h-full flex-col justify-start gap-1">
+            <h2 className="mt-2 text-xl font-semibold text-gray-600">
+              {patient.name}
+            </h2>
+            <p className="text-gray-400">{patient.DOB}</p>
+            <p className="text-gray-400">{`${patient.numberOfVisits}번째 내원`}</p>
+          </div>
+          <p className="ml-40 self-center text-right text-lg">
+            {patient.status}
+          </p>
+        </div>
+      </section>
+      <div className="h-20" />
+      <section>
+        <h2 className="mb-8 text-xl">날짜별 진료기록</h2>
+        <CheckupList checkups={patient.checkups} />
       </section>
     </main>
   );
--- src/types.ts
+++ src/types.ts
@@ -13,4 +13,15 @@ export type Patient = {
   recentVisitDate: string;
   severityPercentage: string;
   status: "HIGH_RISK" | "MODERATE_RISK" | "LOW_RISK" | "NORMAL";
+  checkups: Checkup[];
+};
+
+export type Checkup = {
+  checkupId: number;
+  patientId: number;
+  date: string;
+  symptom: string;
+  status: "HIGH_RISK" | "MODERATE_RISK" | "LOW_RISK" | "NORMAL";
+  ultrasoundImages: string[];
+  report: string;
 };
