7909caea8b1a534846aade9b9ed1e0df2f6c0ad2
yoonchulchung <yoonchul005@gmail.com>
2024-08-17 00:37:26 +0900

    updated

---
+++ Docker/install.sh
@@ -0,0 +1,9 @@
+#############################################################
+# AEYE WEB Compose Builder
+# Created By Yoonchul Chung
+# Created At 2024.08.17
+# Welcome to Visit Github : https://github.com/Yoonchulchung
+#############################################################
+
+docker-compose build --no-cache 2>&1 | tee build.log
+
---
+++ Docker/run.sh
@@ -0,0 +1,29 @@
+#############################################################
+# AEYE Docker WEB Compose Starter
+# Created By Yoonchul Chung
+# Created At 2024.08.17
+# Welcome to Visit Github : https://github.com/Yoonchulchung
+#############################################################
+
+
+initiate_docker_compose()
+{
+  clear
+  figlet WELOCME TO 
+  figlet AEYE WEB
+  cd .. && cd .. && cd Docker && docker-compose up 2>&1 | tee docker-compose.log
+}
+
+npm_install()
+{
+  cd ../AEYE_Front/AEYE_Front/ && npm install
+}
+
+run()
+{
+  npm_install
+
+  initiate_docker_compose
+}
+
+run
